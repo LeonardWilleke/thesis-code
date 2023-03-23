5c4d8726aa1d9342aa0cc1338991c50a5813cddb
Leonard Willeke <st150067@stud.uni-stuttgart.de>
2023-03-23 19:01:18 +0100

    Create new pid controller. It only uses SCALAR variables internally.

---
+++ FMUs/cmake/PIDcontrollerForce/PIDcontrollerForce/config.h
@@ -0,0 +1,46 @@
+#ifndef config_h
+#define config_h
+
+// define class name and unique id
+#define MODEL_IDENTIFIER PIDcontrollerForce
+#define INSTANTIATION_TOKEN "{1AE5E10D-9521-4DE3-80B9-D0EAAA7D5AF1}"
+
+#define CO_SIMULATION
+#define MODEL_EXCHANGE
+
+// define model size
+#define NX 2
+#define NZ 1
+
+#define SET_FLOAT64
+#define GET_OUTPUT_DERIVATIVE
+#define EVENT_UPDATE
+
+#define FIXED_SOLVER_STEP 1e-3
+#define DEFAULT_STOP_TIME 3
+
+typedef enum {
+    vr_time, vr_u, vr_y, vr_r, vr_e, vr_e_ls, vr_kp, vr_ki, vr_kd, vr_P, vr_I, vr_D, vr_force_x, vr_force_y, vr_velocity_x, vr_velocity_y
+} ValueReference;
+
+typedef struct {
+
+    double u;
+    double y;
+    double r;
+    double e;
+    double e_ls;
+    double kp;
+    double ki;
+    double kd;
+    double P;
+    double I;
+    double D;
+    double force_x;
+    double force_y;
+    double velocity_x;
+    double velocity_y;
+
+} ModelData;
+
+#endif /* config_h */
---
+++ FMUs/cmake/PIDcontrollerForce/PIDcontrollerForce/model.c
@@ -0,0 +1,178 @@
+#include <math.h>  // for fabs()
+#include <float.h> // for DBL_MIN
+#include "config.h"
+#include "model.h"
+
+#define V_MIN (0.1)
+#define EVENT_EPSILON (1e-10)
+
+
+void setStartValues(ModelInstance *comp) {
+    M(u)        	= 0;
+    M(y)        	= 0;
+    M(r)        	= 0;
+    M(e_ls)			= 0;
+    M(kp)       	= 0;
+    M(ki)       	= 0;
+    M(kd)       	= 0;
+    M(P)			= 0;
+    M(I)			= 0;
+    M(D)			= 0;
+    M(force_x)      = 0;
+    M(force_y)      = 0;
+    M(velocity_x)   = 0;
+    M(velocity_y)	= 0;
+    
+}
+
+Status calculateValues(ModelInstance *comp) {
+    
+    UNUSED(comp);
+    return OK;
+    
+}
+
+Status getFloat64(ModelInstance* comp, ValueReference vr, double *value, size_t *index) {
+    switch (vr) {
+        case vr_time:
+            value[(*index)++] = comp->time;
+            return OK;
+        case vr_u:
+            value[(*index)++] = M(u);
+            return OK;
+        case vr_y:
+            value[(*index)++] = M(y);
+            return OK;
+        case vr_r:
+            value[(*index)++] = M(r);
+            return OK;
+        case vr_e:
+            value[(*index)++] = M(e);
+            return OK;
+        case vr_e_ls:
+            value[(*index)++] = M(e_ls);
+            return OK;
+        case vr_kp:
+            value[(*index)++] = M(kp);
+            return OK;
+        case vr_ki:
+            value[(*index)++] = M(ki);
+            return OK;
+        case vr_kd:
+            value[(*index)++] = M(kd);
+            return OK;
+        case vr_P:
+            value[(*index)++] = M(P);
+            return OK;
+        case vr_I:
+            value[(*index)++] = M(I);
+            return OK;
+        case vr_D:
+            value[(*index)++] = M(D);
+            return OK;
+        case vr_force_x:
+            value[(*index)++] = M(force_x);
+            return OK;
+        case vr_force_y:
+            value[(*index)++] = M(force_y);
+            return OK;
+        case vr_velocity_x:
+            value[(*index)++] = M(velocity_x);
+            return OK;
+        case vr_velocity_y:
+            value[(*index)++] = M(velocity_y);
+            return OK;
+        default:
+            logError(comp, "Get Float64 is not allowed for value reference %u.", vr);
+            return Error;
+    }
+}
+
+Status setFloat64(ModelInstance* comp, ValueReference vr, const double *value, size_t *index) {
+    switch (vr) {
+
+        case vr_u:
+            M(u) = value[(*index)++];
+            return OK;
+        case vr_y:
+            M(y) = value[(*index)++];
+            return OK;
+        case vr_r:
+            M(r) = value[(*index)++];
+            return OK;
+        case vr_kp:
+            M(kp) = value[(*index)++];
+            return OK;
+        case vr_ki:
+            M(ki) = value[(*index)++];
+            return OK;
+        case vr_kd:
+            M(kd) = value[(*index)++];
+            return OK;
+        case vr_force_x:
+            M(force_x) = value[(*index)++];
+            return OK;
+        case vr_force_y:
+            M(force_y) = value[(*index)++];
+            return OK;
+        default:
+            logError(comp, "Unexpected value reference: %u.", vr);
+            return Error;
+    }
+}
+
+Status getOutputDerivative(ModelInstance *comp, ValueReference valueReference, int order, double *value) {
+
+    if (order != 1) {
+        logError(comp, "The output derivative order %d for value reference %u is not available.", order, valueReference);
+        return Error;
+    }
+
+    switch (valueReference) {
+
+    default:
+        logError(comp, "The output derivative for value reference %u is not available.", valueReference);
+        return Error;
+    }
+    
+    UNUSED(value);
+}
+
+void eventUpdate(ModelInstance *comp) {
+
+    if (false) {
+    
+    } else {
+        comp->valuesOfContinuousStatesChanged = false;
+    }
+
+    comp->nominalsOfContinuousStatesChanged = false;
+    comp->terminateSimulation  = false;
+    comp->nextEventTimeDefined = false;
+}
+
+void getContinuousStates(ModelInstance *comp, double x[], size_t nx) {
+    UNUSED(comp);
+    UNUSED(nx);
+    UNUSED(x);
+}
+
+void setContinuousStates(ModelInstance *comp, const double x[], size_t nx) {
+    UNUSED(comp);
+    UNUSED(nx);
+    UNUSED(x);
+}
+
+void getDerivatives(ModelInstance *comp, double dx[], size_t nx) {
+    UNUSED(comp);
+    UNUSED(nx);
+    UNUSED(dx);
+}
+
+void getEventIndicators(ModelInstance *comp, double z[], size_t nz) {
+
+    UNUSED(nz);
+    UNUSED(comp);
+    UNUSED(z);
+
+}
