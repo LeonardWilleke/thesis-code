e13051499623b3c95c72b2fac778676208102058
Leonard Willeke <st150067@stud.uni-stuttgart.de>
2023-03-29 17:20:51 +0200

    Create working 3-way coupling

--- cases/flow-around-mounted-cylinder-controlled/solid-dummy/solverdummy.py
+++ cases/flow-around-mounted-cylinder-controlled/solid-dummy/solverdummy.py
@@ -15,12 +15,55 @@ except SystemExit:
     print("Usage: python3 ./solverdummy.py precice-config")
     quit()
 
+
+# system parameters
+
+mass 		= 0.03575 # kg
+k_spring 	= 69.48 # N/m
+d_damper 	= 0.0043 # N/s
+
+state_vector_old 	= np.zeros((3,1))
+state_vector 		= np.zeros((3,1))
+
+def update(mass, k_spring, d_damper, state_vector_old, dt, force, displacement_spring):
+	
+    # create system matrix for left hand side
+    system_matrix = np.zeros((3,3))
+    system_matrix[0,0] = 2/dt
+    system_matrix[1,0] = k_spring
+    system_matrix[0,1] = -1
+    system_matrix[1,1] = 2*mass/dt + d_damper
+    system_matrix[2,1] = 2/dt
+    system_matrix[2,2] = -1
+    
+    # create A and c for right hand side
+    A = np.zeros((3,3))
+    A[0,0] = 2/dt
+    A[0,1] = 1
+    A[1,1] = 2*mass/dt
+    A[2,1] = 2/dt
+    A[1,2] = mass
+    A[2,2] = 1
+    
+    c = np.zeros((3,1))
+    c[1,0] = force + k_spring*displacement_spring
+    
+    # compute right hand side
+    right_hand = A * state_vector_old + c
+    
+    # compute values for next time step
+    state_vector = np.linalg.solve(system_matrix, right_hand)
+    
+    return state_vector
+	
+
 configuration_file_name = args.configurationFileName
 
-participant_name = "Spring"
-write_data_name = "Force-Spring"
-read_data_name = "Displacement-Spring"
-mesh_name = "Mesh-Spring"
+participant_name = "Solid"
+write_data_name = "Displacement-Cylinder"
+read_data_name_force = "Force"
+read_data_name_displacement = "Displacement-Spring"
+mesh_name = "Mesh-Solid"
 
 num_vertices = 1  # Number of vertices
 
@@ -40,42 +83,47 @@ vertices = np.zeros((num_vertices, dimensions))
 read_data = np.zeros((num_vertices, dimensions))
 write_data = np.zeros((num_vertices, dimensions))
 
-for x in range(num_vertices):
-    for y in range(0, dimensions):
-        vertices[x, y] = x
-        #read_data[x, y] = x
-        write_data[x, y] = x
-
 
 vertex_ids = interface.set_mesh_vertices(mesh_id, vertices)
-read_data_id = interface.get_data_id(read_data_name, mesh_id)
+read_data_id_force = interface.get_data_id(read_data_name_force, mesh_id)
+read_data_id_displacement = interface.get_data_id(read_data_name_displacement, mesh_id)
 write_data_id = interface.get_data_id(write_data_name, mesh_id)
 
 dt = interface.initialize()
 
 while interface.is_coupling_ongoing():
-    if interface.is_action_required(
-            precice.action_write_iteration_checkpoint()):
-        print("DUMMY: Writing iteration checkpoint")
-        interface.mark_action_fulfilled(
-            precice.action_write_iteration_checkpoint())
+    if interface.is_action_required(precice.action_write_iteration_checkpoint()):
+        
+        state_vector_old_cp = state_vector_old
+        state_vector_cp = state_vector
+        
+        interface.mark_action_fulfilled(precice.action_write_iteration_checkpoint())
 
-    read_data = interface.read_block_vector_data(read_data_id, vertex_ids)
-    print(read_data)
-
+    read_data_force = interface.read_block_vector_data(read_data_id_force, vertex_ids)
+    force = read_data_force[0,1]
+    read_data_displacement = interface.read_block_vector_data(read_data_id_displacement, vertex_ids)
+    displacement_spring = read_data_displacement[0,1]
+	
+    # compute next time step
+    state_vector = update(mass, k_spring, d_damper, state_vector_old, dt, force, displacement_spring)
+	
+	
     write_data[0,0] = 0
-    write_data[0,1] = 0 # 5*read_data[0,1]
+    write_data[0,1] = state_vector[0,0]
 
     interface.write_block_vector_data(write_data_id, vertex_ids, write_data)
 
-    print("DUMMY: Advancing in time")
+    print("Solid: Advancing in time")
     dt = interface.advance(dt)
 
-    if interface.is_action_required(
-            precice.action_read_iteration_checkpoint()):
-        print("DUMMY: Reading iteration checkpoint")
-        interface.mark_action_fulfilled(
-            precice.action_read_iteration_checkpoint())
+    if interface.is_action_required(precice.action_read_iteration_checkpoint()):
+        
+        state_vector_old = state_vector_old_cp
+        state_vector = state_vector_cp
+        
+        interface.mark_action_fulfilled(precice.action_read_iteration_checkpoint())
+    else:
+    	state_vector_old = state_vector
 
 interface.finalize()
-print("DUMMY: Closing python solver dummy...")
+print("Solid: Closing python solver dummy...")
